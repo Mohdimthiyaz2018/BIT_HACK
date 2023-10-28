357443b5d32cb4f981aaf2874139e43bd88350ba
ShrithiK <ShrithiK.ig19@bitsathy.ac.in>
2023-10-28 21:48:24 +0530

    ProfilePage drafr

---
+++ Pages/Profile/profile.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="HTML, CSS, JavaScript" />
+    <meta name="author" content="Shrithik" />
+    <link rel="stylesheet" href="profile.css" />
+    <title>SkillsBIT</title>
+  </head>
+  <body>
+    <div class="header">
+      <div>
+        <img
+          class="Logo"
+          src="../../Asserts/Images/bitFullLogo.webp"
+          alt="BIT Full Logo"
+        />
+      </div>
+      <div class="navBar">
+        <a href="../Home/homeA.php" class="active">HOME</a>
+        <a href="../homeA/homeA.php">ABOUT</a>
+        <a href="../Domain/domain.php">PROBLAM-STATEMENTS</a>
+        <a href="../Profile/profile.php">PROFILE</a>
+        <a href="../Home/homeB.php" class="login">LOG OUT</a>
+      </div>
+    </div>
+    <div class="body">
+    <div class="title">PROFILE</div>
+        <div class="box">
+            <div class="profile">
+                <div>
+                    <img src="../../Asserts/Images/proPicPlaceHolder.jpg" alt="Profile Picture" width="300px" height="300px">
+                </div>
+                <div class="proDitl">
+                    <div class="subHead">Name</div>
+                    <div class="subHead">:</div>
+                    <div class="subBody">ShrithiK</div>
+                    <div class="subHead">RollNumber</div>
+                    <div class="subHead">:</div>
+                    <div class="subBody">191IG133</div>
+                    <div class="subHead">Email</div>
+                    <div class="subHead">:</div>
+                    <div class="subBody">shrithik.ig19@bitsathy.ac.in</div>
+                    <div class="subHead">PhoneNO</div>
+                    <div class="subHead">:</div>
+                    <div class="subBody">8610646600</div>
+                    <div class="subHead">Problam Statement</div>
+                    <div class="subHead">:</div>
+                    <div class="subBody">Not Selected Yet!</div>
+                </div>
+
+            </div>
+            
+
+        </div>
+    </div>
+  </body>
+</html>
